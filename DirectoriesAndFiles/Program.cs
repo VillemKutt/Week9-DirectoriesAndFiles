a7c8a9cf248cbf13320fd5a8b6b8fe161404f79d
Villem <villemkutt@gmail.com>
2023-05-02 23:05:42 +0300

    Add project files.

---
+++ DirectoriesAndFiles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoriesAndFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\...\samples";
+            Console.WriteLine("Enter directory name:");
+
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine();
+
+
+            if (System.IO.Directory.Exists($"{rootDirectory}\\{newDirectory}") && System.IO.File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and File exist.");
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+    }
+}
